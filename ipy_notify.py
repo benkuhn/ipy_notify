8b877e39a2b84a1b0f8812569e6864a0b661fb81
Ben Kuhn <ben.s.kuhn@gmail.com>
2014-08-26 21:23:14 -0700

    first draft of script

---
+++ ipy_notify.py
@@ -0,0 +1,65 @@
+import IPython.display
+
+import datetime
+import os
+import subprocess
+
+NOTIFY_SCRIPT = IPython.display.HTML("""
+<script>
+if (Notification.permission == 'granted') {
+    var notification = new Notification("IPython command finished", {
+        icon: "http://icons.iconarchive.com/icons/cornmanthe3rd/plex/512/Other-python-icon.png",
+        body: IPython.notebook.notebook_name
+    });
+    notification.onclick = function () {
+        window.focus();
+    }
+}
+</script>
+""")
+
+ACTIVATE_SCRIPT = IPython.display.HTML("""
+<button class="notify_button" onclick="Notification.requestPermission(); $('.notify_button').hide();" style="display:none;">
+  Show notifications
+</button>
+<script>
+if (Notification.permission == 'default') {
+    $('.notify_button').show();
+}
+</script>
+""")
+
+class Timer(object):
+    def __init__(self, shell):
+        self.shell = shell
+        self.functions = [
+            ('pre_execute', self.pre_execute),
+            ('post_execute', self.post_execute),
+        ]
+        self.time = None
+        IPython.display.display(ACTIVATE_SCRIPT)
+
+    def pre_execute(self):
+        self.time = datetime.datetime.now()
+
+    def post_execute(self):
+        if self.time is None:
+            return
+        then = self.time
+        self.time = None
+        now = datetime.datetime.now()
+        diff = (now - then)
+        if diff > datetime.timedelta(seconds=5):
+            IPython.display.display(NOTIFY_SCRIPT)
+
+timer = None
+
+def load_ipython_extension(ipython):
+    global timer
+    timer = Timer(ipython)
+    for hook, fn in timer.functions:
+        ipython.events.register(hook, fn)
+
+def unload_ipython_extension(ipython):
+    for hook, fn in timer.functions:
+        ipython.events.unregister(hook, fn)
